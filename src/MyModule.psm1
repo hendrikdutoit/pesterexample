b4e449c3740334550ebb901be72bed3a8b685346
Hendrik du Toit <hendrik@brightedge.co.za>
2024-10-18 15:26:45 +0200

    Mock Parametrized for input example test

--- src/MyModule.psm1
+++ src/MyModule.psm1
@@ -7,4 +7,13 @@ function Get-Hello {
 function Invoke-CalledFunction {
     Write-Output "Called by Get-Hello"
 }
-Export-ModuleMember -Function Get-Hello, Invoke-CalledFunction
+
+function Get-ConsoleInput {
+    param (
+        [Object]$textPrompt
+    )
+    $result = Read-Host $textPrompt
+    return $result
+}
+
+Export-ModuleMember -Function Get-ConsoleInput, Get-Hello, Invoke-CalledFunction
